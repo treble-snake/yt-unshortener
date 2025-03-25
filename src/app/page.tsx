54ba27804abbbb023d81c198f06e1e318277c000
ksolo1 <ksolo@alpha-sense.com>
2025-03-25 18:47:23 +0200

    feat: implement web app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,55 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import React from 'react';
+import {Button, Flex, Input, Layout, Space, Typography} from "antd";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const [url, setUrl] = React.useState<string>('');
+    const [unshortenedUrl, setUnshortenedUrl] = React.useState<string>('');
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    const transformUrl = (url: string) => {
+        if (url.includes("youtube.com/shorts/")) {
+            const videoId = url.split("/shorts/")[1].split("?")[0];
+            setUnshortenedUrl(`https://youtube.com/watch/?v=${videoId}`)
+        } else if (url.length === 0) {
+            setUnshortenedUrl('')
+        } else {
+            setUnshortenedUrl('Input is not a YouTube Shorts URL')
+        }
+    }
+
+    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUrl(e.target.value)
+        transformUrl(e.target.value)
+    }
+    const paste = () => {
+        navigator.clipboard.readText().then((text) => {
+                setUrl(text)
+                transformUrl(text)
+            }
+        )
+    }
+
+    return (
+        <Layout style={{padding: '24px', width: '100%', height: '100%'}}>
+            <Flex style={{width: '100%', height: '100%'}} justify={'center'}
+                  align={'center'}>
+                <Space direction={'vertical'}>
+                    <Space>
+                        <Input
+                            placeholder="Short video URL" value={url}
+                            allowClear
+                            onChange={onInputChange}/>
+                        <Button type="primary" onClick={paste}>Paste
+                            Url</Button>
+                    </Space>
+                    <Space>
+                        <Typography.Text>Unshortened URL:</Typography.Text>
+                        <Typography.Text
+                            copyable>{unshortenedUrl}</Typography.Text>
+                    </Space>
+                </Space>
+            </Flex>
+        </Layout>
+    );
 }
