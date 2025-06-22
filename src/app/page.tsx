7a0709b42806eb32b3ef977128741bc5ecf40146
ksolo69 <ksolo@alpha-sense.com>
2025-06-22 16:36:15 +0300

    feat: add auto-copy and improve layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,141 @@
 'use client'
 
 import React from 'react';
-import {Button, Flex, Input, Layout, Space, Typography} from "antd";
+import {
+  Button,
+  Checkbox,
+  Flex,
+  Input,
+  Layout,
+  Popover,
+  Space,
+  Typography
+} from "antd";
+import {QuestionCircleOutlined} from "@ant-design/icons";
 
 export default function Home() {
-    const [url, setUrl] = React.useState<string>('');
-    const [unshortenedUrl, setUnshortenedUrl] = React.useState<string>('');
+  const [url, setUrl] = React.useState<string>('');
+  const [isError, setIsError] = React.useState<boolean>(false);
+  const [isCopied, setIsCopied] = React.useState<boolean>(false);
+  const [copyError, setCopyError] = React.useState<string | null>(null);
+  const [unshortenedUrl, setUnshortenedUrl] = React.useState<string>('');
+  const [autoCopy, setAutoCopy] = React.useState<boolean>(true);
 
-    const transformUrl = (url: string) => {
-        if (url.includes("youtube.com/shorts/")) {
-            const videoId = url.split("/shorts/")[1].split("?")[0];
-            setUnshortenedUrl(`https://youtube.com/watch/?v=${videoId}`)
-        } else if (url.length === 0) {
-            setUnshortenedUrl('')
-        } else {
-            setUnshortenedUrl('Input is not a YouTube Shorts URL')
-        }
+  const {Compact} = Space;
+  const {Text, Paragraph} = Typography;
+
+  const transformUrl = (url: string) => {
+    setIsError(false)
+    setCopyError(null)
+    setIsCopied(false)
+
+    if (url.includes("youtube.com/shorts/")) {
+      const videoId = url.split("/shorts/")[1].split("?")[0];
+      const result = `https://youtube.com/watch/?v=${videoId}`;
+      setUnshortenedUrl(result)
+
+      if (autoCopy) {
+        navigator.clipboard
+          .writeText(result)
+          .then(() => setIsCopied(true))
+          .catch((e) => setCopyError(e))
+      }
+
+      return
     }
 
-    const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUrl(e.target.value)
-        transformUrl(e.target.value)
+    if (url.length === 0) {
+      return setUnshortenedUrl('')
     }
-    const paste = () => {
-        navigator.clipboard.readText().then((text) => {
-                setUrl(text)
-                transformUrl(text)
+
+    setIsError(true);
+    setUnshortenedUrl('Input is not a YouTube Shorts URL')
+  }
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUrl(e.target.value)
+    transformUrl(e.target.value)
+  }
+
+  const paste = () => {
+    navigator.clipboard.readText().then((text) => {
+      setUrl(text)
+      transformUrl(text)
+    })
+  }
+
+  const popover = (
+    <div style={{maxWidth: 400}}>
+      <Paragraph>
+        I needed it to share my lifting videos with my coach for form checks.
+        Shorts are convenient for quick upload, but not so much for review.
+      </Paragraph>
+      <Paragraph>
+        If you leave the checkbox below checked, the result will be
+        automatically copied to your clipboard, so you can paste it straight
+        into
+        your messenger or whatever.
+      </Paragraph>
+    </div>
+  )
+
+  return (
+    <Layout style={{padding: '24px', width: '100%', height: '100%'}}>
+      <Flex style={{width: '100%', height: '100%'}} justify={'center'}
+            align={'center'}>
+        <Space direction={'vertical'} style={{textAlign: 'center'}}>
+          <Text type={'secondary'}>
+            Enter a short YouTube video URL and get a
+            link to the same video as a regular (not short) one.
+          </Text>
+          <Popover content={popover}>
+            <Button type="dashed" shape="round"
+                    icon={<QuestionCircleOutlined/>}>
+              What? Why?
+            </Button>
+          </Popover>
+
+          <Compact>
+            <Input
+              placeholder="Short video URL" value={url}
+              allowClear
+              onChange={onInputChange}
+              status={
+                isError ? 'error' : undefined
+              }
+            />
+            <Button type="primary" onClick={paste}>Paste URL</Button>
+          </Compact>
+          <Checkbox
+            checked={autoCopy}
+            onChange={() => setAutoCopy(!autoCopy)}
+          >
+            <Text italic type={'secondary'}>Auto-copy result to clipboard</Text>
+          </Checkbox>
+          <Space direction={'vertical'}>
+            {
+              isError && <Text
+                type="danger">Error: {unshortenedUrl}</Text>
             }
-        )
-    }
-
-    return (
-        <Layout style={{padding: '24px', width: '100%', height: '100%'}}>
-            <Flex style={{width: '100%', height: '100%'}} justify={'center'}
-                  align={'center'}>
-                <Space direction={'vertical'}>
-                    <Space>
-                        <Input
-                            placeholder="Short video URL" value={url}
-                            allowClear
-                            onChange={onInputChange}/>
-                        <Button type="primary" onClick={paste}>Paste
-                            Url</Button>
-                    </Space>
-                    <Space>
-                        <Typography.Text>Unshortened URL:</Typography.Text>
-                        <Typography.Text
-                            copyable>{unshortenedUrl}</Typography.Text>
-                    </Space>
-                </Space>
-            </Flex>
-        </Layout>
-    );
+            {!isError && unshortenedUrl && (
+              <div>
+                <Text type={'secondary'}>Unshortened URL:</Text>
+                <br/>
+                <Text copyable>{unshortenedUrl}</Text>
+              </div>
+            )
+            }
+            {
+              isCopied && <Text type="success">Copied to clipboard!</Text>
+            }
+            {
+              copyError && <Text type="danger">
+                Please copy the result manually, clipboard error: {copyError}
+              </Text>
+            }
+          </Space>
+        </Space>
+      </Flex>
+    </Layout>
+  );
 }
